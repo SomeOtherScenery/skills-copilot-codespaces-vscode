1e9b8170c57e4adce1f7a00a48d276ce8d2711d6
Chenfeng Wang <220224490@seu.edu.cn>
2024-11-08 05:51:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,9 @@
-// create a web server
+// Create web server
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// create a route to comments
 app.get('/comments', (req, res) => {
-  res.send('This is a page of comments');
+    res.send('Comments');
 });
-
-// start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-// Run the server and visit http://localhost:3000/comments in your browser. You should see the message This is a page of comments.
